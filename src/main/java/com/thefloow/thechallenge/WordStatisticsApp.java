fdf11a532a03ca0f2670adf0f5978c7fc4a8e229
Leigh Ciechanowski <lciech@hotmail.co.uk>
2017-07-28 11:12:05 +0100

    And so it begins! Created an empty maven project with gitignore

---
+++ src/main/java/com/thefloow/thechallenge/WordStatisticsApp.java
@@ -0,0 +1,19 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package com.thefloow.thechallenge;
+
+/**
+ *
+ * @author lciechanowski
+ */
+public class WordStatisticsApp {
+   
+    public static void main( String[] args )
+    {
+         System.out.println("And so it begins");
+    }
+    
+}
